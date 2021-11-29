765da95daa5922affacf1cff5f90694f483d08a0
leejiihyeon <leejiihyeon@gmail.com>
2021-11-29 11:23:08 +0900

    .

--- JAVA/L04.java
+++ JAVA/L04.java
@@ -3,14 +3,16 @@ package JAVA;
 public class L04 {
 
 	public static void main(String[] args) {
+		
 		int i=5;
-		while(i<=5) {
+		while(i>=0) {
 			int j=1;
-			while(j<=i);
-			System.out.print("*");
-			j++;
+			while(j<=i) {
+				System.out.print("*");
+				j++;
+			}
+			System.out.println();
+			i--;
 		}
-		System.out.println();
-		i++;
 	} //메인 종료
 } //클래스 종료
