d67ad798a836fd85e57d19acdc7fcb62453af605
leejiihyeon <leejiihyeon@gmail.com>
2021-11-30 12:17:39 +0900

    .

---
+++ JAVA/L16.java
@@ -0,0 +1,15 @@
+package JAVA;
+
+public class L16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		
+		for(int i=1 ; i <=10 ; i++) {
+			sum+=i;
+				System.out.println("1~10까지의 합 : "+sum);
+			
+		}
+	}
+}
