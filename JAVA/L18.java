9bdd815b0e5db1af089e851b9eee110e5399a6e3
leejiihyeon <leejiihyeon@gmail.com>
2021-12-01 11:17:12 +0900

    .

---
+++ JAVA/L18.java
@@ -0,0 +1,19 @@
+package JAVA;
+
+public class L18 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sum=0;
+		int i=1;
+		while(i<=10) {
+			int j=1;
+			while(j<=i);
+			System.out.println("1부터 10까지의 합 : "+sum);
+			j++;
+		}
+		System.out.println();
+		i++;
+	}
+}
