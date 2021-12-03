6cdcd136143b2b4e63fa465d20f3717b5fbae9df
leejiihyeon <leejiihyeon@gmail.com>
2021-12-03 10:36:19 +0900

    .

---
+++ JAVA/L30.java
@@ -0,0 +1,14 @@
+package JAVA;
+
+public class L30 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sum=0;
+		for(int i=1 ; i<=10 ; i++) {
+			sum+=i;
+		}
+		System.out.println("1에서 10까지의 총합 : "+sum);
+	}
+}
