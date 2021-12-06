8531ccc1e9fdf58cd03062b3bdc415e604c3f369
leejiihyeon <leejiihyeon@gmail.com>
2021-12-06 16:52:26 +0900

    .

---
+++ JAVA/L41.java
@@ -0,0 +1,13 @@
+package JAVA;
+
+public class L41 {
+
+	public static void main(String[] args) {
+		
+		String name="홍길동";
+		String job="프로게이머";
+		System.out.println(name);
+		System.out.println(job);
+
+	}
+}
