d9d636c115254b56ff49969bb0c0027837d467b9
leejiihyeon <leejiihyeon@gmail.com>
2021-12-06 16:41:16 +0900

    유니코드

---
+++ JAVA/L39.java
@@ -0,0 +1,20 @@
+package JAVA;
+
+public class L39 {
+
+	public static void main(String[] args) {
+		
+		char var1='A'; //유니코드 : 65
+		char var2='B'; //유니코드 : 66
+		char var3='가'; //유니코드 : 44032
+		char var4='각'; //유니코드 : 44033
+		
+		int var5='A';
+		
+		char c=65; //10진수
+		char c1=0x0041; //16진수
+		
+		System.out.println(var5);
+	}
+
+}
