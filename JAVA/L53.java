9fa5c0226c966434159f09625fadbe631345ed1e
leejiihyeon <leejiihyeon@gmail.com>
2021-12-07 12:49:43 +0900

    .

---
+++ JAVA/L53.java
@@ -0,0 +1,13 @@
+package JAVA;
+
+public class L53 {
+
+	public static void main(String[] args) {
+		
+		int intValue = 10;
+		double doubleValue = 5.5;
+		double result = intValue + doubleValue;
+		System.out.println(result);
+		
+	}
+}
