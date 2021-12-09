1400d7a0e39f005a74695dbb2bc865572b2eb8b8
leejiihyeon <leejiihyeon@gmail.com>
2021-12-09 12:10:30 +0900

    .

---
+++ JAVA/L61.java
@@ -0,0 +1,17 @@
+package JAVA;
+
+public class L61 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("[for문을 이용한 구구단]");
+		
+		for(int i=1 ; i<=9 ; i++) {
+			for(int j=1 ; j<=9 ; j++) {
+				System.out.println(i+"*"+j+"="+i*j);
+			}
+			System.out.println();
+		}
+	}
+}
