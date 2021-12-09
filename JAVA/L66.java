34871091d9cfd5acadaf4537a15c893ada09e6d6
leejiihyeon <leejiihyeon@gmail.com>
2021-12-09 12:36:55 +0900

    .

---
+++ JAVA/L66.java
@@ -0,0 +1,15 @@
+package JAVA;
+
+public class L66 {
+
+	public static void main(String[] args) throws Exception {
+		
+		int keyCode;
+		
+		
+		while(true) {
+			keyCode = System.in.read();
+			System.out.println("keyCode : " + keyCode);
+		}
+	}
+}
