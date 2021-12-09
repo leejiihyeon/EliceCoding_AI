9e32b0c73224cce6ff7c850969dec3cf3219cbf9
leejiihyeon <leejiihyeon@gmail.com>
2021-12-09 12:37:37 +0900

    .

---
+++ JAVA/L65.java
@@ -0,0 +1,23 @@
+package JAVA;
+
+import java.io.IOException;
+
+public class L65 {
+
+	public static void main(String[] args) throws Exception {
+		
+		//int keyCode = System. + in. + read();
+
+		int keyCode;
+		
+		keyCode = System.in.read();
+		System.out.println("keyCode : " + keyCode);
+		
+		keyCode = System.in.read();
+		System.out.println("keyCode : " + keyCode);
+
+		keyCode = System.in.read();
+		System.out.println("keyCode : " + keyCode);
+
+	}
+}
