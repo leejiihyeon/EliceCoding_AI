619da2627761b8a7cea9ad630c94033e78f042b4
leejiihyeon <leejiihyeon@gmail.com>
2021-12-09 12:17:06 +0900

    .

---
+++ JAVA/L63.java
@@ -0,0 +1,17 @@
+package JAVA;
+
+public class L63 {
+
+	public static void main(String[] args) {
+		
+		int value = 123;
+		System.out.printf("상품의 가격:%d\n", value);
+		System.out.printf("상품의 가격:%6d\n", value);
+		System.out.printf("상품의 가격:%-6d\n", value);
+		System.out.printf("상품의 가격:%06d\n", value);
+		
+		
+		
+	}
+
+}
