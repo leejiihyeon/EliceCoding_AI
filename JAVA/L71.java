623bb25b7b8a4fed1e31c276c35736de217f3e9b
leejiihyeon <leejiihyeon@gmail.com>
2021-12-10 12:15:54 +0900

    .

--- JAVA/L71.java
+++ JAVA/L71.java
@@ -12,13 +12,8 @@ public class L71 {
 		int age = 25;
 		String tel1="010", tel2="123", tel3="4567";
 		
-		System.out.println(name);
-		System.out.print(age);
-		//System.out.printf();
-				
-		
-		
-		
+		System.out.println("이름 : "+name);
+		System.out.print("나이 : "+age+"\n");
+		System.out.printf("전화 : %1$s-%2$s-%3$s", tel1, tel2, tel3);
 	}
-
 }
