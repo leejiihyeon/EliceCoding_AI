f4ad5e99d9d60d92188f52394a64d8125874f3c1
leejiihyeon <leejiihyeon@gmail.com>
2021-12-10 12:09:13 +0900

    .

---
+++ JAVA/L71.java
@@ -0,0 +1,24 @@
+package JAVA;
+/*
+이름 : 김자바
+나이 : 25
+전화 : 010-123-4567
+*/
+public class L71 {
+
+	public static void main(String[] args) {
+		
+		String name = "김자바";
+		int age = 25;
+		String tel1="010", tel2="123", tel3="4567";
+		
+		System.out.println(name);
+		System.out.print(age);
+		//System.out.printf();
+				
+		
+		
+		
+	}
+
+}
