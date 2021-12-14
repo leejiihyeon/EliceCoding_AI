437890d30b29a7743b356c56cc7b50e83b2b1ec8
leejiihyeon <leejiihyeon@gmail.com>
2021-12-14 18:03:32 +0900

    .

--- JAVA/L85.java
+++ JAVA/L85.java
@@ -3,6 +3,17 @@ package JAVA;
 public class L85 {
 
 	public static void main(String[] args) {
-
+		System.out.println("[while문 - 구구단]");
+		
+		int i=1;
+		while(i<9) {
+			i++;
+			int j=0;
+			while(j<9) {
+				j++;
+				System.out.println(i+"*"+j+"="+i*j);
+			}
+			System.out.println();
+		}
 	}
 }
