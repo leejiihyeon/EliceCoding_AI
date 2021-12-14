a6e8ff2e27a0d68e69c77ffbf19c7b53ca984851
leejiihyeon <leejiihyeon@gmail.com>
2021-12-14 18:01:03 +0900

    .

--- JAVA/L84.java
+++ JAVA/L84.java
@@ -5,9 +5,9 @@ public class L84 {
 	public static void main(String[] args) {
 		
 		int sum=0;
-		for(i<=10) {
+		for(int i=1; i<=10 ; i++) {
 			sum+=i;
 		}
-		System.out.println(sum);
+		System.out.println("1~10 총합"+sum);
 	}
 }
