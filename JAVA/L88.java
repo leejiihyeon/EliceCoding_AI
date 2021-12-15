7d3a02602626bf3a8e6447a1b12474f941d67d0f
leejiihyeon <leejiihyeon@gmail.com>
2021-12-15 17:24:13 +0900

    .

--- JAVA/L88.java
+++ JAVA/L88.java
@@ -7,7 +7,12 @@ public class L88 {
 /*
 String=>byte
 String str = "10";
-byte value = Byte.parseByte(str);		
+byte value = Byte.parseByte(str);
+*/
+		
+/*
+Integer.parseInt() : 문자열을 정수 int 타입으로 변환합니다
+Double.parseDouble() : 문자열을 실수 double 타입으로 변환합니다
 */
 
 	}
