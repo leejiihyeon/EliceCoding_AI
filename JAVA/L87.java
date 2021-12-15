da012044537cb71dfc9c54c4cab8a8237588538d
leejiihyeon <leejiihyeon@gmail.com>
2021-12-15 12:00:46 +0900

    .

--- JAVA/L87.java
+++ JAVA/L87.java
@@ -1,24 +1,8 @@
 package JAVA;
 
-import java.io.IOException;
-
 public class L87 {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		
-		int num = 10;
-		
-		if(num%2) {
-			System.out.println("잘못된 조건식 입니다.");
-			
-			
-		}
-		if(num) {
-			System.out.println("잘못된 조건식 입니다.");
-		}
-		
-		if(num%2==0) {
-			System.out.printf("%d는 짝수입니다", num);
-		}
 	}
 }
