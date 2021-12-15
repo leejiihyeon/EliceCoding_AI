88ee9161b2878776fabac7746b13782a8f3834a6
leejiihyeon <leejiihyeon@gmail.com>
2021-12-15 11:51:20 +0900

    .

--- JAVA/L86.java
+++ JAVA/L86.java
@@ -7,6 +7,15 @@ public class L86 {
 		
 		System.out.println("while문 - 구구단");
 		
-		
+		int i=1;
+		while(i<=9) {
+			i++;
+			int j=0;
+			while(j<9) {
+				j++;
+				System.out.println(i+"*"+j+"="+i*j);
+			}
+			System.out.println();
+		}
 	}
 }
