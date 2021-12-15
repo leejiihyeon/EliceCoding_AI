6fa786b8306fd175ce2c0270e891ebfb61e7e319
leejiihyeon <leejiihyeon@gmail.com>
2021-12-15 17:16:54 +0900

    .

--- JAVA/L88.java
+++ JAVA/L88.java
@@ -4,5 +4,21 @@ public class L88 {
 
 	public static void main(String[] args) {
 
+/*
+String=>byte
+String str = "10";
+byte value = Byte.parseByte(str);		
+*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
