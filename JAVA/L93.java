9b607ed35650eb2a0d0159b6c30fe4c3e8e1eaf8
leejiihyeon <leejiihyeon@gmail.com>
2021-12-20 08:57:54 +0900

    .

--- JAVA/L93.java
+++ JAVA/L93.java
@@ -4,5 +4,12 @@ public class L93 {
 
 	public static void main(String[] args) {
 
+		int x = -100;
+		int result1 = +x;
+		int result2 = -x;
+		
+		System.out.println(result1);
+		System.out.println(result2);
+		
 	}
 }
