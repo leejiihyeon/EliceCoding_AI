fa386143315d30fe38bfe8af460cf8880fcf26a2
leejiihyeon <leejiihyeon@gmail.com>
2021-12-20 08:56:57 +0900

    .

--- JAVA/L92.java
+++ JAVA/L92.java
@@ -3,5 +3,12 @@ package JAVA;
 public class L92 {
 	public static void main(String[] args) {
 		
+		int i1 = +100;
+		int i2 = -100;
+		double d1 = +3.14;
+		double d2 = -10.5;
+
+//일반적으로 부호 연산자를 다음과 같이 정수 및 실수 리터럴 앞에 붙여 양수 및 음수를 표현한다.
+		
 	}
 }
