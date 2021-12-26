3ab11686e37d149f8b049a4d68938ce9aa47e298
leejiihyeon <leejiihyeon@gmail.com>
2021-12-26 12:23:57 +0900

    .

--- JAVA/L95.java
+++ JAVA/L95.java
@@ -4,5 +4,12 @@ public class L95 {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("[1~1000까지의 합 구하기]");
+		
+		for(int i=1 ; i<=1000 ; i++) {
+			for(int j=1 ; j<=1000; j++) {
+				
+			}
+		}
 	}
 }
