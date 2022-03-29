6ee8b3222b4b410a49675c1d30d3de9ab8a60b69
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-03-29 09:22:00 +0900

    Add files via upload

--- NaveraiApplication.java
+++ NaveraiApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 import cfr.FaceController;
+import objectdetection.ObjectDetectionService;
 
 @SpringBootApplication
 //현재패키지 다른 설정 기본 인식
 @ComponentScan //현재패키지 다른 설정 인식
 @ComponentScan(basePackageClasses = FaceController.class)
+@ComponentScan(basePackageClasses = ObjectDetectionService.class)
 
 public class NaveraiApplication {
 
