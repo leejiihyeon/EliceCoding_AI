609329974859c7e00cffc517fd8875e490cc9083
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-03-29 09:22:53 +0900

    Add files via upload

---
+++ ObjectDetectionService.java
@@ -0,0 +1,90 @@
+package objectdetection;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.springframework.stereotype.Service;
+
+import edu.multi.boot.NaverService;
+
+//ai서비스별 서비스클래스중 어느 서비스
+@Service("objectdetectionservice")
+public class ObjectDetectionService implements NaverService {
+
+	@Override
+	public String test(String image) {
+		StringBuffer response = null;
+		StringBuffer reqStr = new StringBuffer();
+        String clientId = "c5yrs0p1qt";//애플리케이션 클라이언트 아이디값";
+        String clientSecret = "JrMkw5U2S5KBOZu1cI4mwr3lO0TvGzmizs6jAc7v";//애플리케이션 클라이언트 시크릿값";
+
+        try {
+            String paramName = "image"; // 파라미터명은 image로 지정
+            String imgFile = "C:\\Users\\ASUS\\OneDrive\\바탕 화면\\STUDY\\ai_images\\ai_images/" + image;
+            File uploadFile = new File(imgFile);
+            String apiURL = "https://naveropenapi.apigw.ntruss.com/vision-obj/v1/detect"; // 객체 인식
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setUseCaches(false);
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            // multipart request
+            String boundary = "---" + System.currentTimeMillis() + "---";
+            con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+            con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", clientId);
+            con.setRequestProperty("X-NCP-APIGW-API-KEY", clientSecret);
+            OutputStream outputStream = con.getOutputStream();
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream, "UTF-8"), true);
+            String LINE_FEED = "\r\n";
+            // file 추가
+            String fileName = uploadFile.getName();
+            writer.append("--" + boundary).append(LINE_FEED);
+            writer.append("Content-Disposition: form-data; name=\"" + paramName + "\"; filename=\"" + fileName + "\"").append(LINE_FEED);
+            writer.append("Content-Type: "  + URLConnection.guessContentTypeFromName(fileName)).append(LINE_FEED);
+            writer.append(LINE_FEED);
+            writer.flush();
+            FileInputStream inputStream = new FileInputStream(uploadFile);
+            byte[] buffer = new byte[4096];
+            int bytesRead = -1;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            outputStream.flush();
+            inputStream.close();
+            writer.append(LINE_FEED).flush();
+            writer.append("--" + boundary + "--").append(LINE_FEED);
+            writer.close();
+            BufferedReader br = null;
+            int responseCode = con.getResponseCode();
+            if(responseCode==200) { // 네이버 object detection 정상 호출
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {  // 오류 발생
+                System.out.println("error!!!!!!! responseCode= " + responseCode);
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            }
+            String inputLine;
+            if(br != null) {
+                response = new StringBuffer();
+                while ((inputLine = br.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                br.close();
+                System.out.println(response.toString()); //서버 결과 출력
+            } else {
+                System.out.println("error !!!");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return response.toString();
+	}
+
+}
