2199cf507896b4710e5a99f575915394baca826a
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-04-02 15:56:42 +0900

    Add files via upload

--- ObjectDetectionController.java
+++ ObjectDetectionController.java
@@ -10,30 +10,31 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ObjectDetectionController {
-	
+
 	@Autowired
 	@Qualifier("objectdetectionservice")
 	ObjectDetectionService service;
 	
 	@RequestMapping("/objectdetectioninput")
 	public ModelAndView objectdetectioninput() {
-		File f = new File("C://Users//ASUS//OneDrive//바탕 화면//STUDY//ai_images//ai_images");
+		File f = new File("C:\\Users\\student\\Desktop\\ai_images");
 		String [] filelist = f.list();
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("objectdetectioninput");
 		mv.addObject("filelist", filelist);
 		return mv;
 		
-		
 	}
 	
-	@RequestMapping("/objectdetectionresult")
-	public ModelAndView objectdetectionresult(String image) { //${param.image}
-		String json = service.test(image); //id secetkey, url - json
+	@RequestMapping("/objectdetectionsult")
+	public ModelAndView objectdetectionsult(String image) {//${param.image}
+		String json = service.test(image);//id secretkey, url - json
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("objectdetectioninput");
-		mv.addObject("objectdetectioninput", json);
-		return mv;
+		mv.setViewName("objectdetectionresult");
+		mv.addObject("objectdetectionresult", json);
+		return mv;		
 	}
-
+	
+	
+	
 }
