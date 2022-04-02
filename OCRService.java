b502eb646421f43097bec290e259fc413995c575
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-04-02 15:57:10 +0900

    Add files via upload

---
+++ OCRService.java
@@ -0,0 +1,119 @@
+package ocr;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.UUID;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import edu.multi.boot1.NaverService;
+
+@Service("ocrservice")
+public class OCRService implements NaverService{
+
+	@Override
+	public String test(String fontFile) {
+		//image 지역변수 이미 선언 상태.
+		StringBuffer response = new StringBuffer();
+
+		//API URL SECRET KEY 
+		
+		String apiURL = "https://pma7o2tj8s.apigw.ntruss.com/custom/v1/14883/0bffcc41d1277b95e689e6c50f9178347fe5cb4fa019041217afb68be9ee601a/general";
+		String secretKey = "U0loQWZEckp6WFpBVHVQc3RYRWFOSkJhbk1rZWNoVEU=";
+		String imageFile = "C:\\Users\\student\\Desktop\\ai_images\\" + fontFile;
+
+		try {
+			URL url = new URL(apiURL);
+			HttpURLConnection con = (HttpURLConnection)url.openConnection();
+			con.setUseCaches(false);
+			con.setDoInput(true);
+			con.setDoOutput(true);
+			con.setReadTimeout(30000);
+			con.setRequestMethod("POST");
+			String boundary = "----" + UUID.randomUUID().toString().replaceAll("-", "");
+			con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+			con.setRequestProperty("X-OCR-SECRET", secretKey);
+
+			JSONObject json = new JSONObject();
+			json.put("version", "V2");
+			json.put("requestId", UUID.randomUUID().toString());
+			json.put("timestamp", System.currentTimeMillis());
+			JSONObject image = new JSONObject();
+			image.put("format", "jpg");
+			image.put("name", "demo");
+			JSONArray images = new JSONArray();
+			images.put(image);
+			json.put("images", images);
+			String postParams = json.toString();
+
+			con.connect();
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			long start = System.currentTimeMillis();
+			File file = new File(imageFile);
+			writeMultiPart(wr, postParams, file, boundary);
+			wr.close();
+
+			int responseCode = con.getResponseCode();
+			BufferedReader br;
+			if (responseCode == 200) {
+				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			} else {
+				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+			}
+			String inputLine;
+			while ((inputLine = br.readLine()) != null) {
+				response.append(inputLine);
+			}
+			br.close();
+
+			System.out.println(response);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return response.toString();
+	}//test end
+
+	private static void writeMultiPart(OutputStream out, String jsonMessage, File file, String boundary) throws
+		IOException {
+		StringBuilder sb = new StringBuilder();
+		sb.append("--").append(boundary).append("\r\n");
+		sb.append("Content-Disposition:form-data; name=\"message\"\r\n\r\n");
+		sb.append(jsonMessage);
+		sb.append("\r\n");
+
+		out.write(sb.toString().getBytes("UTF-8"));
+		out.flush();
+
+		if (file != null && file.isFile()) {
+			out.write(("--" + boundary + "\r\n").getBytes("UTF-8"));
+			StringBuilder fileString = new StringBuilder();
+			fileString
+				.append("Content-Disposition:form-data; name=\"file\"; filename=");
+			fileString.append("\"" + file.getName() + "\"\r\n");
+			fileString.append("Content-Type: application/octet-stream\r\n\r\n");
+			out.write(fileString.toString().getBytes("UTF-8"));
+			out.flush();
+
+			try (FileInputStream fis = new FileInputStream(file)) {
+				byte[] buffer = new byte[8192];
+				int count;
+				while ((count = fis.read(buffer)) != -1) {
+					out.write(buffer, 0, count);
+				}
+				out.write("\r\n".getBytes());
+			}
+
+			out.write(("--" + boundary + "--\r\n").getBytes("UTF-8"));
+		}
+		out.flush();
+	}//writeMultipart end
+}
