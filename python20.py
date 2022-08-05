3496ef780f698663c7d66a6ede41847ac060b5dd
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-08-05 15:55:38 +0900

    *

---
+++ python20.py
@@ -0,0 +1,6 @@
+my_dict = {"사과":"apple", "바나나":"banana", "당근":"carrot"} # "사과":"apple", "바나나":"banana", "당근":"carrot" 의 관계를 my_dict 딕셔너리에 넣어보세요.
+
+var1 = my_dict["사과"] # key가 "사과"인 value를 var1에 저장해보세요.
+
+del my_dict["당근"] # del을 이용하여 key가 "당근"인 원소를 my_dict에서 제거해보세요.
+my_dict["체리"] = "cherry" # key가 "체리"이고, value가 "cherry"인 원소를 my_dict에 추가해보세요.
