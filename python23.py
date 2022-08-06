1e7b9b63e88ec174bb5a79ed76f1426dac16b438
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-08-07 01:13:34 +0900

    Add files via upload

---
+++ python23.py
@@ -0,0 +1,10 @@
+# 함수를 정의하기 위해 def 를 이용하세요.
+def plus_print(a, b):
+
+    print(a+b) # a+b 를 출력하세요.
+    
+def times_return(a, b):
+    return a*b # return을 이용하여 a*b를 반환하세요.
+    
+plus_print(3, 4)
+print(times_return(3, 4))
