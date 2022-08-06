a02bc889141b211e590955e76394f00a690eb3c6
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-08-06 22:23:34 +0900

    Add files via upload

---
+++ python18.py
@@ -0,0 +1,10 @@
+# 인덱싱 : 시퀀스의 한 원소를 가져오는 것
+# 슬라이싱 : 시퀀스의 일부분을 가져오는 것
+
+my_str ="Impossible" # "Impossible" 문자열을 my_str에 저장해보세요.
+my_list = ["Apple", "Banana", "Chamwae", "Durian"] # ["Apple","Banana","Chamwae","Durian"] 을 my_list에 저장해보세요.
+
+var1 = my_list[2] # 인덱싱을 이용하여 my_list 속 "Chamwae"을 선택해보세요.
+var2 = my_str[2:] # 슬라이싱을 이용하여 my_str 에서 "possible" 문자열만 선택해보세요.
+
+print(var1,var2)
