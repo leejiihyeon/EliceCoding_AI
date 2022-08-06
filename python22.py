9dd0173875ba45c16d47dbcf566d38db9906e9cf
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-08-07 01:11:36 +0900

    Add files via upload

---
+++ python22.py
@@ -0,0 +1,7 @@
+my_list = [13, 11, 9, 7, 5] # 원하는 숫자 5개가 담긴 리스트를 선언해보세요.
+
+var1 = sum(my_list) # sum을 이용하여 my_list의 합을 구해보세요.
+
+var2 = len(my_list) # len을 이용하여 my_list의 길이를 구해보세요.
+
+var3 = var1/var2 # 합(var1)을 길이(var2)로 나눠 평균을 구해보세요.
