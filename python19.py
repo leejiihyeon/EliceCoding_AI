31b62de2c167420dd5b42d4ced16c3c769b6b311
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-08-06 22:23:45 +0900

    Add files via upload

---
+++ python19.py
@@ -0,0 +1,7 @@
+my_str ="Impossible"
+my_list = ["Apple","Banana","Chamwae","Durian"]
+
+var1 = len(my_str) # my_str 의 길이를 var1에 저장하세요.
+var2 = "Egg" in my_list # in 을 이용하여 "Egg"가 my_list 안에 있는지 확인해보세요.
+
+print(var1,var2)
