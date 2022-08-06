a60cf0e65e542024041c15adaf37d777f0db913d
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-08-06 22:23:13 +0900

    Add files via upload

---
+++ python16.py
@@ -0,0 +1,8 @@
+my_list = []
+my_list.append(5) # my_list 에 5를 추가해보세요.
+my_list.append(4) # my_list 에 4를 추가해보세요.
+my_list.append(2) # my_list 에 2를 추가해보세요.
+my_list.append(1) # my_list 에 1를 추가해보세요.
+
+
+print(my_list) #[5, 4, 2, 1]
