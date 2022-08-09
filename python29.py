1e7fbbfddb10a1c4ed4466960a0bedc3cf597094
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-08-09 09:44:48 +0900

    Add files via upload

---
+++ python29.py
@@ -0,0 +1,11 @@
+## 이렇게 해보세요!를 따라 수행해보세요.
+# from, import를 활용하여 random 모듈에서 randrange 함수를 불러오세요.
+# from a import b : a 모듈(혹은 패키지)에서 b 함수를 가져오겠다. b를 사용하기 위해선 b() 꼴 로 사용 가능
+from random import randrange 
+
+# math 모듈을 불러오세요.
+# import a : a 모듈을 불러오겠다. a 모듈 안에있는 b 함수를 사용하기 위해선 a.b() 꼴로 사용해야함
+import math
+
+var1 = randrange(1, 11)
+var2 = math.log(5184, 72) # math.log 함수를 사용해보세요.
