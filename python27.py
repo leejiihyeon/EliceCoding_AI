a4a1b7eebc39cdb1a6f3c4da8a2bd05b642c8ec0
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-08-09 09:39:52 +0900

    *

---
+++ python27.py
@@ -0,0 +1,11 @@
+## 이렇게 해보세요!를 따라 수행해보세요.
+import 28
+
+var1 = cal.modelName # cal.modelName을 var1에 저장하세요.
+
+var2 = cal.plus(3, 4) # cal.plus를 활용하여 3+4를 구해보세요.
+
+var3 = cal.minus(7, 2) # cal.minus를 활용하여 7-2를 구해보세요.
+
+## 변수의 값을 확인하는 출력문입니다.
+print(var1, var2, var3)
---
+++ python28.py
@@ -0,0 +1,7 @@
+def plus(a, b):
+    return a+b # a+b 를 반환하세요.
+
+def minus(a, b):
+    return a-b # a-b 를 반환하세요.
+
+modelName = 'ELI-C2' # "ELI-C2" 를 modelName에 저장하세요.
